c56e0292812d1bdb762495db52373c0e3ef4f126
Matthew Alaniz <alaniz.matthew07@gmail.com>
2019-08-23 15:43:47 -0500

    making a dice game with DOM practice

---
+++ js/DomPractice.js
@@ -0,0 +1,9 @@
+//==================================== DOM practice ===========================================
+let scores,roundScore, activePlayer, dice;
+
+ scores = [0, 0];
+ roundScore = 0;
+ activePlayer = 0;
+ dice = Math.floor(Math.random() * 6) + 1;
+
+ document.querySelector('#score-' + activePlayer).textContent = dice;
