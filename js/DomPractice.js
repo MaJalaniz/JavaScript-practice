af134103651a7ad848fe40da8c2245e9d51eadd0
Matthew Alaniz <alaniz.matthew07@gmail.com>
2019-08-23 15:54:51 -0500

    arranging card and setting font size

--- js/DomPractice.js
+++ js/DomPractice.js
@@ -6,4 +6,5 @@ let scores,roundScore, activePlayer, dice;
  activePlayer = 0;
  dice = Math.floor(Math.random() * 6) + 1;
 
- document.querySelector('#score-' + activePlayer).textContent = dice;
+ //document.querySelector('#score-' + activePlayer).textContent = dice;
+document.querySelector('#score-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
