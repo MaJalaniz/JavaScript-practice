7386b597ef1e21e57c9d993354ba48d1dcbb823a
Matthew Alaniz <alaniz.matthew07@gmail.com>
2019-08-23 17:32:29 -0500

    stuck on separating dice from current score

--- js/DomPractice.js
+++ js/DomPractice.js
@@ -1,10 +1,17 @@
 //==================================== DOM practice ===========================================
-let scores,roundScore, activePlayer, dice;
+let scores,roundScore, activePlayer, dice, playerScore;
 
  scores = [0, 0];
  roundScore = 0;
  activePlayer = 0;
+ playerScore = 0;
+
  dice = Math.floor(Math.random() * 6) + 1;
 
- //document.querySelector('#score-' + activePlayer).textContent = dice;
-document.querySelector('#score-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+
+
+ document.querySelector('#score-' + activePlayer).textContent = dice;
+
+
+let x = document.querySelector('#score-' + activePlayer).textContent;
