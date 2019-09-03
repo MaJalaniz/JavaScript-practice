87e5693dee037f1d297ba655e5a2c1ffe5e85c71
Matthew Alaniz <alaniz.matthew07@gmail.com>
2019-09-03 11:15:12 -0500

    query selectors, elementByClassName and forEach

--- js/domHtml.js
+++ js/domHtml.js
@@ -1,5 +1,35 @@
 "use strict";
-const searchBar = document.forms['search-books'].query['input'];
+/*
+let titles = document.getElementsByClassName('title');
+
+Array.from(titles).forEach(function(item) {
+    console.log(item);
+});
+*/
+
+
+
+const smw = document.querySelector('#game-list li:nth-child(2) .name');
+//const wrap = document.querySelector('');
+
+console.log(smw);
+
+
+let games = document.querySelectorAll('#game-list li .name');
+console.log(games);
+
+Array.from(games).forEach(function(game){
+    console.log(game)
+   //game.textContent = 'test';
+});
+
+
+
+
+
+
+/*
+const searchBar = document.forms['search-games'].query['input'];
 searchBar.addEventListener('keyup', function(e){
     const term = e.target.value.toLowerCase();
     const books = list.getElementsByTagName('li');
@@ -11,4 +41,7 @@ searchBar.addEventListener('keyup', function(e){
            book.style.display = 'none';
        }
     });
+
+
 });
+*/
