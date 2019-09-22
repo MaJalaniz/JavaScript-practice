0bb25fe7f1d64900c602ce532e5d89a2bfbbfb2d
Matthew Alaniz <alaniz.matthew07@gmail.com>
2019-09-22 18:01:31 -0500

    function for area of a triangle

--- js/JavaScriptChallengeEasy.js
+++ js/JavaScriptChallengeEasy.js
@@ -1,24 +1,38 @@
 //=====================Easy Challenges on Edabit========================
 
+/*
+//Return the Sum of two numbers
 
 function Sum(one, two){
     return one + two;
 }
 console.log(Sum(5,5));
-
+*/
 //==============================================================
+/*
+//Return the first element in an array
+
 var numbers = [1500,2,3,4,5,6,7,8,9];
-
-function firstElement(){
-    return numbers[0];
-}
-console.log(firstElement());
-
-
-function first(){
-    for(var i = 0; i < numbers.length; i++){
-        console.log(numbers[i]);
+var otherNum = [2000, 45, 34, 24];
+function first(num){
+    for(var i = 0; i < num.length; i++){
+        if(num[i] === num[0]){
+            console.log(num[i]);
+        }
     }
 }
 first(numbers);
+first(otherNum);
+*/
 //==============================================================
+
+//Write a function that takes in a Base and Height and returns the
+//area of a triangle.
+
+function triangle(base, height){
+    return (base * height)/2;
+}
+triangle(20,20);
+
+//===============================================================
+
