49923b84c4ddb8114165fb6471078c683bd55ac8
Matthew Alaniz <alaniz.matthew07@gmail.com>
2019-09-26 19:30:01 -0500

    is today Christmas?

--- js/JavaScriptChallengeEasy.js
+++ js/JavaScriptChallengeEasy.js
@@ -24,8 +24,18 @@ console.log(numbers([0,1,2,3,4,5,400, 300]));
 
 //======================================================================
 
+    //Write a function that checks if today is Christmas
 
+function christmas(month, day) {
+var christ = new Date("December 25");
 
+    if((month === christ.getMonth()+1) && day === christ.getDate()) {
+        return "Yay today is Christmas woooo!";
+    }else {
+        return "We have to wait until then"
+    }
+}
+console.log(christmas(11,25));
 
 
 //======================================================================
